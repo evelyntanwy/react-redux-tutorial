b689eb03f8cc71bfdc973c88232350327395ffca
Evelyn Tan <evetan.wy@gmail.com>
2022-02-19 20:36:02 +1300

    Intial setup with redux store

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Profile from "./components/Profile";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <h2>React - Redux</h2>
+      <Profile />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
